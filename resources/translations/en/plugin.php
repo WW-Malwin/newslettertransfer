015001556f97e9dd9b428ec0a33ba22fe159df4b
Malwin <malwinrudek@gmail.com>
2024-05-19 08:03:49 +0200

    menu change

--- resources/translations/en/plugin.php
+++ resources/translations/en/plugin.php
@@ -4,5 +4,9 @@ return [
     'plugin_name' => 'Newsletter Transfer Plugin',
     'plugin_description' => 'Transfers newsletter recipients from PlentyMarkets to Emailsys.',
     'menu_label' => 'Newsletter Transfer Plugin',
-    'menu_description' => 'Manage the transfer of newsletter recipients.'
+    'menu_description' => 'Manage the transfer of newsletter recipients.',
+    'Config.credentialsTab' => 'Credentials',
+    'Config.apiEndpointLabel' => 'API Endpoint',
+    'Config.usernameLabel' => 'Username',
+    'Config.passwordLabel' => 'Password'
 ];
--- src/Providers/NewsletterTransferServiceProvider.php
+++ src/Providers/NewsletterTransferServiceProvider.php
@@ -3,12 +3,6 @@
 namespace NewsletterTransfer\Providers;
 
 use Plenty\Plugin\ServiceProvider;
-use Plenty\Plugin\ConfigRepository;
-use Plenty\Plugin\Events\Dispatcher;
-use Plenty\Modules\Plugin\Menu\Contracts\MenuRepositoryContract;
-use Plenty\Modules\Plugin\Menu\Events\LoadMenu;
-use NewsletterTransfer\Providers\NewsletterTransferRouteServiceProvider;
-use NewsletterTransfer\Providers\NewsletterTransferConfigServiceProvider;
 
 class NewsletterTransferServiceProvider extends ServiceProvider
 {
@@ -16,21 +10,5 @@ class NewsletterTransferServiceProvider extends ServiceProvider
     {
         $this->getApplication()->register(NewsletterTransferRouteServiceProvider::class);
         $this->getApplication()->register(NewsletterTransferConfigServiceProvider::class);
-    }
-
-    public function boot(MenuRepositoryContract $menuRepository, Dispatcher $eventDispatcher)
-    {
-        // Menüeintrag unter "Einstellungen" hinzufügen
-        $eventDispatcher->listen(LoadMenu::class, function (LoadMenu $event) use ($menuRepository) {
-            $menuRepository->addSubMenu(
-                'menu.settings',  // Dies ist der Menübereich "Einstellungen"
-                'NewsletterTransfer',
-                [
-                    'name' => 'Newsletter Transfer Plugin',
-                    'url' => '/newsletter-transfer/config',
-                    'icon' => 'fa-envelope'
-                ]
-            );
-        });
     }
 }
